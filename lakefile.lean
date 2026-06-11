9bdcef34a786fed63ab3072b775fc6b4cb55ce1a
Jonathan ƒ(n) Reed <jonathanalanreed1983@gmail.com>
2026-06-10 22:09:26 -0400

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -1,10 +1,8 @@
 import Lake
 open Lake DSL
 
-package "collatz"
+package «collatz» where
 
 @[default_target]
-lean_lib «Collatz_#1»
-
-require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git" @ "v4.26.0"
+lean_lib «collatz» where
+  srcDir := "."
